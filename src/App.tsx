e90aa0e64b95115c227ccbdf2849bcf772cd91f3
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-02-20 21:09:13 -0700

    Add Preahvihear font and update styles in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-full h-full mx-auto flex ">
+      <div className="pt-50 pl-50  flex flex-row gap-4">
+        <div className="w-50 h-auto">
+          <img src="src/assets/hero/meTest.png" />
+        </div>
+        <div className="w-max flex flex-col">
+          <span>A front end programmer who</span>
+          <div className="flex w-140">
+            <h1 className="text-[50px]">With great power comes great <span>responsibility</span></h1>
+          </div>
+          <span>I really like Spiderman</span>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
   )
 }
 
