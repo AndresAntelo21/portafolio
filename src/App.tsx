98270d163d3b64161f7ef8707b8677910614299d
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-02-20 21:32:27 -0700

    Refactor layout in App component to enhance image positioning and add background gradient

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,11 @@ function App() {
   return (
     <div className="w-full h-full mx-auto flex ">
       <div className="pt-50 pl-50  flex flex-row gap-4">
-        <div className="w-50 h-auto">
-          <img src="src/assets/hero/meTest.png" />
+        <div className="absolute top-40 left-40 w-65 h-65 rounded-full
+  bg-gradient-to-r from-[#763CAC]/40 to-[#320F85]/40 blur-lg opacity-90 -z-10">
+        </div>
+        <div className="w-50 h-auto relative">
+          <img src="src/assets/hero/meTest.png" className="relative" />
         </div>
         <div className="w-max flex flex-col">
           <span className="font-[Preahvihear] text-[20px]">A front end programmer who</span>
