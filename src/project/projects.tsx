5a5d8da0699ce547fefbd42de00ee897f2e21e3a
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-02-23 17:52:23 -0700

    Update layout and dimensions in Projects component for improved responsiveness

--- src/project/projects.tsx
+++ src/project/projects.tsx
@@ -12,9 +12,9 @@ interface ProjectsProps {
 const Projects: React.FC<ProjectsProps> = ({ reverse = false, title, description, imageSrc, featuredText, icons }) => {
     return (
         <div className="relative flex">
-            <div className={`absolute top-[-40px] ${reverse ? 'left-[-40px]' : 'right-[-100px]'} w-80 h-95 rounded-full bg-gradient-to-r from-[#763CAC]/40 to-[#320F85]/40 blur-lg opacity-90 -z-10`}></div>
+            <div className={`absolute top-[-40px] ${reverse ? 'left-[-10px]' : 'right-[-50px]'} w-95 h-95 rounded-full bg-gradient-to-r from-[#763CAC]/40 to-[#320F85]/40 blur-lg opacity-90 -z-10`}></div>
             <div className={`flex ${reverse ? 'flex-row-reverse' : 'flex-row'}`}>
-                <div className={`w-[657px] h-auto flex flex-row z-10 relative ${reverse ? 'pl-[215px]' : 'left-[70px]'}`}>
+                <div className={`w-[657px] h-auto flex flex-row z-10 relative ${reverse ? 'pl-[230px]' : 'left-[70px]'}`}>
                     <div className={`flex-col w-[372px] flex ${reverse ? 'items-end' : 'items-start'}`}>
                         <h2 className="font-[poppins] text-[16px] text-[#9857D3]">{featuredText}</h2>
                         <h3 className="font-[poppins] text-[34px]">{title}</h3>
@@ -30,9 +30,9 @@ const Projects: React.FC<ProjectsProps> = ({ reverse = false, title, description
                         </div>
                     </div>
                 </div>
-                <div className={`relative w-[400px] h-[250px] bg-[#1e012c] rounded-[10px] overflow-hidden z-0 flex items-end pt-[10px] ${reverse ? 'pr-[20px]' : 'pl-[20px]'}`}>
+                <div className={`relative w-[450px] h-[300px] bg-[#1e012c] rounded-[10px] overflow-hidden z-0 flex items-end pt-[10px] ${reverse ? 'pr-[30px]' : 'pl-[20px]'} ${reverse ? 'ml-[60px]' : 'ml-[0]'}`}>
                     <div className={`relative pt-4 w-full h-full overflow-hidden ${reverse ? 'pr-4' : 'pl-4'}`}>
-                        <img src={imageSrc} className={`w-[400px] h-[250px] object-cover ${reverse ? 'rounded-tr-[16px]' : 'rounded-tl-[16px]'}`} />
+                        <img src={imageSrc} className={`w-[500px] h-[300px] object-cover ${reverse ? 'rounded-tr-[16px]' : 'rounded-tl-[16px]'}`} />
                     </div>
                 </div>
             </div>
