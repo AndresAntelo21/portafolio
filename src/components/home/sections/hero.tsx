aa68c665fd36679064c1c898527a65a6824bc7a3
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-03-05 23:47:23 -0700

    Update Hero component with new layout, text, and background elements

--- src/components/home/sections/hero.tsx
+++ src/components/home/sections/hero.tsx
@@ -1,7 +1,22 @@
 export const Hero = () => {
   return (
-    <div>
-      <h1>Hola soy /</h1>
+    <div className="font-poppins flex flex-col items-center gap-4 px-12 py-4 text-center text-lg">
+      <h1>A Software Developer</h1>
+      <span className="text-3xl font-bold">
+        Where logic meets creativity...
+      </span>
+      <h2 className="text-xs">
+        Because every great app starts with a single line of code
+      </h2>
+      <div className="relative mt-10">
+        <div className="absolute top-1/2 left-1/2 z-0 h-60 w-60 -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-violet-500 blur-2xl"></div>
+        <div className="absolute top-1/2 left-1/2 z-0 h-30 w-30 -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-amber-50 blur-2xl"></div>
+        <img
+          src="/public/hero/test-img.png"
+          alt="yo merito"
+          className="relative z-10 w-50 pt-5"
+        />
+      </div>
     </div>
   );
 };
