784ac9abf23d4114865f0dfa09523e2ae427b6d5
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-03-16 01:37:44 -0700

    Remove unused Hero component import from App.tsx for cleaner code

--- src/router/App.tsx
+++ src/router/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "@/components/home/home";
 import { Navigator } from "@/components/navigator/navigator";
-// import { Hero } from "@/components/home/sections/hero/hero";
 import { WorkExperience } from "@/components/home/sections/work-experience/work-experience";
 import { Projects } from "@/components/home/sections/projects/projects";
 import { Foooter } from "@/components/footer/footer";
