f8b973ae217c038bdbeb102787c1f8f3d882b123
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-03-16 00:57:55 -0700

    Refactor RevoltWeb component: update technology icons, enhance project description, and improve layout for better clarity and presentation

--- src/projects/revolt/revolt-web.tsx
+++ src/projects/revolt/revolt-web.tsx
@@ -1,9 +1,6 @@
 import { ToolsText } from "@/components/home/sections/tools/tools-text";
 import { RevoltCarousel } from "@/projects/revolt/revolt-carousel";
-import { FaReact, FaGithub } from "react-icons/fa";
-import { RiTailwindCssFill } from "react-icons/ri";
-import { SiVite } from "react-icons/si";
-import { BiLogoTypescript } from "react-icons/bi";
+import { FaGithub, FaVuejs, FaJs } from "react-icons/fa";
 import { FaLink } from "react-icons/fa6";
 // CODIGO SUCIO - PASAR TODO A UN COMPONENTE NO HAY TIEMPO, SOLO ES UNA DEMOSTRACION
 // QUIEN LEA ESTO ES UN METICHE MUEJEJEJE
@@ -19,7 +16,7 @@ export const RevoltWeb = () => {
         />
       </div>
       {/* cuadro de tecnologias */}
-      <div className="flex flex-col gap-5 lg:flex-row-reverse">
+      <div className="flex w-full flex-col gap-5 lg:flex-row-reverse">
         <div className="flex h-max w-full flex-col rounded-md border-1 border-violet-800 py-4 pr-4 lg:w-[40%]">
           <div className="w-full">
             <div className="flex w-max items-center justify-center rounded-r-md bg-violet-800 px-4 py-2">
@@ -29,10 +26,8 @@ export const RevoltWeb = () => {
             </div>
           </div>
           <div className="mt-10 flex flex-wrap gap-2 p-2">
-            <ToolsText title="React" icons={[FaReact]} />
-            <ToolsText title="Tailwind" icons={[RiTailwindCssFill]} />
-            <ToolsText title="Typescript" icons={[BiLogoTypescript]} />
-            <ToolsText title="Vite" icons={[SiVite]} />
+            <ToolsText title="Vue.js" icons={[FaVuejs]} />
+            <ToolsText title="JavaScript" icons={[FaJs]} />
           </div>
           <div>
             <div className="w-full pt-10">
@@ -43,7 +38,7 @@ export const RevoltWeb = () => {
               </div>
             </div>
             <div className="flex gap-2 pt-4 pl-2 text-xl">
-              <h3 className="">November -</h3>
+              <h3 className="">December 2024 -</h3>
               <span className="italic">Present</span>
             </div>
           </div>
@@ -57,7 +52,8 @@ export const RevoltWeb = () => {
             </div>
             <div className="flex flex-col gap-5 pt-4 pl-4 font-medium">
               <a
-                href="/"
+                href="https://github.com/Legrafica/solarfuel_front"
+                target="_blank"
                 className="flex justify-center rounded-lg bg-violet-800/70 py-1"
               >
                 <span className="flex items-center gap-2 text-lg uppercase">
@@ -66,7 +62,8 @@ export const RevoltWeb = () => {
                 </span>
               </a>
               <a
-                href="/"
+                href="https://revoltmx.com/"
+                target="_blank"
                 className="flex justify-center rounded-lg bg-violet-700/30 py-1"
               >
                 <span className="flex items-center gap-2 text-lg uppercase">
@@ -77,52 +74,81 @@ export const RevoltWeb = () => {
             </div>
           </div>
         </div>
-        <div className="flex w-full flex-col gap-2 p-2 text-left lg:w-[60%]">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
+        <div className="font-poppins flex w-full flex-col gap-8 text-left lg:w-[60%]">
+          <div className="w-full rounded-md bg-violet-800 py-2 text-center text-2xl tracking-widest">
+            <span>Project Description</span>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p>
+              <strong>Revolt</strong> is a company focused on{" "}
+              <strong>sustainable energy solutions</strong>, offering solar
+              panel systems that help homes and businesses reduce electricity
+              costs while minimizing their environmental impact. With a focus on
+              innovation and efficiency, Revolt aims to provide clean and
+              accessible technology to drive a more sustainable future.
+            </p>
+            <p>
+              The <strong>Revolt</strong> landing page is designed to showcase
+              the company, its values, and the benefits of adopting solar
+              energy, while also making it easy for potential customers to get
+              in touch.
+            </p>
+          </div>
+          <div className="w-full rounded-md bg-violet-800 py-2 text-center text-2xl tracking-widest">
+            <span>Features</span>
+          </div>
+          <div>
+            <ul className="ml-5 flex list-disc flex-col gap-2">
+              <li>
+                <strong>Modern and attractive design</strong>: A visually
+                engaging interface that reinforces the brand’s identity.
+              </li>
+              <li>
+                <strong>Clear and structured information</strong>: Explains the
+                advantages of solar energy and how Revolt can help clients
+                transition.
+              </li>
+              <li>
+                <strong>Conversion-optimized layout</strong>: Strategically
+                designed sections to guide users toward requesting information
+                or a quote.
+              </li>
+              <li>
+                <strong>Responsive design</strong>: Ensuring smooth navigation
+                on any device, including mobile, tablet, and desktop.
+              </li>
+              <li>
+                <strong>Quick contact options</strong>: Integrated forms and
+                direct links for seamless communication with the company.
+              </li>
+            </ul>
+          </div>
+
+          <div className="w-full rounded-md bg-violet-800 py-2 text-center text-2xl tracking-widest">
+            <span> How It Works?</span>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p>
+              The <strong>Revolt</strong> landing page is developed using{" "}
+              <strong>Vue.js</strong>, a JavaScript framework that enables fast
+              and dynamic user interfaces. To handle smooth navigation between
+              sections without page reloads, <strong>Vue Router</strong> is
+              used, ensuring a seamless browsing experience.
+            </p>
+            <p>
+              The interface design is built with{" "}
+              <strong>pure HTML and CSS</strong>, maintaining a clean and
+              professional look. Additionally, <strong>JavaScript</strong>{" "}
+              enhances interactivity and optimizes the user experience, making
+              information easily accessible and engaging.
+            </p>
+            <p>
+              With this technology stack, the <strong>Revolt</strong> website
+              effectively presents its solar energy solutions while making it
+              easy for customers to reach out and start their transition to
+              sustainable energy.
+            </p>
+          </div>
         </div>
       </div>
     </div>
