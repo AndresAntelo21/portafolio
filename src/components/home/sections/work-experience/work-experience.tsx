a0042df8bd558a99f1ca5badafff5b92f2c32d14
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2026-05-02 21:51:40 -0700

    chore: update work experience images to new formats and add Vado ExperienceCard

--- src/components/home/sections/work-experience/work-experience.tsx
+++ src/components/home/sections/work-experience/work-experience.tsx
@@ -1,7 +1,5 @@
-
 import { useLocation } from "react-router-dom";
 import { ExperienceCard } from "../projects/experience-card";
-
 
 interface WorkExperienceProps {
   className?: string;
@@ -23,33 +21,38 @@ export const WorkExperience: React.FC<WorkExperienceProps> = ({
           WORK <span className="text-blue-primary">EXPERIENCE</span>
         </SectionHeading>
       </div>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 w-full">
+      <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         <ExperienceCard
-          imageSrc="/work-experience/unison/unison-logo.webp"
-          hoverImageSrc="/work-experience/unison/unison-hover.webp"
+          imageSrc="/work-experience/unison/unisonv2.png"
+          hoverImageSrc="/work-experience/unison/unisonv2-hover.png"
           linkUrl="/unison"
         />
         <ExperienceCard
-          imageSrc="/work-experience/csipro/csi-logo.webp"
-          hoverImageSrc="/work-experience/csipro/csi-hover.webp"
+          imageSrc="/work-experience/csipro/csiprov2.png"
+          hoverImageSrc="/work-experience/csipro/csiprov2-hover.png"
           linkUrl="/csipro"
         />
         <ExperienceCard
-          imageSrc="/work-experience/telsoluciones/telsoluciones-logo.webp"
-          hoverImageSrc="/work-experience/telsoluciones/bg-telsoluciones.webp"
+          imageSrc="/work-experience/telsoluciones/telsolucionesv2.png"
+          hoverImageSrc="/work-experience/telsoluciones/telsolucionesv2-hover.png"
           linkUrl="/telsoluciones"
         />
         <ExperienceCard
-          imageSrc="/work-experience/legrafica/legrafica-logo.webp"
-          hoverImageSrc="/work-experience/legrafica/legrafica-hover.webp"
+          imageSrc="/work-experience/legrafica/legraficav2.png"
+          hoverImageSrc="/work-experience/legrafica/legraficav2-hover.png"
           linkUrl="/legrafica"
         />
         <ExperienceCard
-          imageSrc="/work-experience/vincco/bg-vincco.webp"
-          hoverImageSrc="/work-experience/vincco/bg-site.webp"
+          imageSrc="/work-experience/vincco/vinccov2.png"
+          hoverImageSrc="/work-experience/vincco/vinccov2-hover.png"
           linkUrl="/vincco"
         />
+        <ExperienceCard
+          imageSrc="/work-experience/vado/vado.png"
+          hoverImageSrc="/work-experience/vado/vado-hover.png"
+          linkUrl="/vado"
+        />
       </div>
-    </div >
+    </div>
   );
 };
